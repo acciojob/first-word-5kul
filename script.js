e188a3acade641f958b9e29867b210de0e2d9790
KULDEEP MISHRA <158719940+5kul@users.noreply.github.com>
2025-07-25 11:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
-  // your code here
+  if (!s) return ''; // If string is empty or null
+
+  s = s.trimStart(); // Remove leading spaces
+  const spaceIndex = s.indexOf(' ');
+
+  // If there's no space, return the entire string
+  return spaceIndex === -1 ? s : s.slice(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
